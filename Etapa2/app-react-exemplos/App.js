f5d4ba86116e9d4bb46d4c8fb950b176c83fafe8
thiaguim163 <thiagohenriquedr16@gmail.com>
2025-04-29 10:11:31 -0300

    CRUD simples: addItem

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -1,31 +1,57 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { StyleSheet, Text, View, Button, Image, TextInput } from "react-native";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
 
+  /// CRUD em memoria
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState("");
+  const [editItemId, setEditItemId] = useState(null);
+  const [editItemText, setEditItemText] = useState("");
+
   const incrementCounter = () => {
-    setCounter = counter + 1;
+    setCounter(counter + 1);
   };
-
   const decrementCounter = () => {
-    setCounter = counter - 1;
+    setCounter(counter - 1);
   };
 
+  const addItem = () => {
+    if (text.trim() === "") {
+      return;
+    }
+    const newItem = {
+      id: Math.random().toString(),
+      text: text.trim(),
+    };
+    setItems([...items, newItem]);
+    setText("");
+    console.log(items);
+  };
   return (
     <View style={styles.container}>
-      <Text>Olá App React Native - Atualiza!</Text>
-      <Image
-        source={{ uri: "https://picsum.photos/200" }}
-        style={{ width: 200, height: 200 }}
+      <TextInput
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        placeholder="Enter text item"
       />
+      <Button title="add Item" onPress={addItem} />
+
+      <Text style={styles.text}>ola app react native!</Text>
+      <Image
+        soure={{ url: "https://picsum.photo/200" }}
+        style={{ widht: 200, height: 200 }}
+      />
+
       <StatusBar style="auto" />
       <Text style={styles.text}>Counter: {counter}</Text>
 
       <View style={styles.buttonContainer}>
-        <Button title="increment" onPress={incrementCounter} />
-        <Button title="decrement" onPress={decrementCounter} />
+        <Button title="Increment" onPress={incrementCounter} />
+        <Button title="descrement" onPress={decrementCounter} />
       </View>
     </View>
   );

1a0ccc1233904b4da99d4afd397dbd8a091fc23a
thiaguim163 <thiagohenriquedr16@gmail.com>
2025-05-20 08:26:01 -0300

    ScrollViewExample.

--- Etapa2/app-react-exemplos/App3.js
+++ Etapa2/app-react-exemplos/App3.js
@@ -1,11 +1,21 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import ScrollViewExamples from "./components/ScrollViewExamples";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TextInput,
+  FlatList,
+} from "react-native";
+
+import Inputs from "./components/Inputs";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ScrollViewExamples />
+      <Inputs />
     </View>
   );
 }

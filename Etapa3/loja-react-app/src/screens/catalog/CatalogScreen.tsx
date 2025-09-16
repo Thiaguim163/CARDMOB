23f7b8b28881319d8ee019997f1c3eea5d8b2802
Thiago <thiagohenriquedr16@gmail.com>
2025-09-16 08:17:42 -0300

    Tela de catálogo com backend

--- Etapa3/loja-react-app/src/screens/catalog/CatalogScreen.tsx
+++ Etapa3/loja-react-app/src/screens/catalog/CatalogScreen.tsx
@@ -1,28 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react"; // alterado
 import { View, Text, FlatList, StyleSheet } from "react-native";
 
 import CatalogCard from "./CatalogCard";
 
 // Todo: importar o serviço de recuperação do catalog
+import { getCatalog } from "../../services/catalogService"; // novo
 
 const CatalogScreen = ({ navigation }: any) => {
+  const [catalog, setCatalog] = useState<any[]>([]); // novo
+
+  // bloco novo
+  useEffect(() => {
+    const fetchCatalog = async () => {
+      try {
+        const data = await getCatalog();
+        setCatalog(data);
+      } catch (error) {
+        console.error("Erro ao buscar o catálogo:", error);
+      }
+    };
+    fetchCatalog();
+    console.log(catalog);
+  }, []);
+
   const handleBuyPress = (product: any) => {
     // 1 - Adicionar ao carrinho
     // 2 - Ir para a tela do carrinho
     console.log(product);
   };
 
-  const renderItem = ({ product }: any) => (
-    <CatalogCard product={product} onBuyPress={() => handleBuyPress(product)} />
+  const renderItem = (
+    { item }: any // alterado
+  ) => (
+    <CatalogCard
+      product={item} // alterado
+      onBuyPress={() => handleBuyPress(item)} // alterado
+    />
   );
 
   return (
     <View style={styles.container}>
       <Text>Menu</Text>
       <FlatList
-        data={[]}
+        data={catalog} // alterado
         renderItem={renderItem}
-        keyExtractor={(item: any) => item.id}
+        keyExtractor={(item: any) => item.id.toString()} // alterado
       />
     </View>
   );

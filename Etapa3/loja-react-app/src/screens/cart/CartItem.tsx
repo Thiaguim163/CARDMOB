e43f423b4b83e57f7d3a2189cd7a1bfbcb8f7350
Thiago <thiagohenriquedr16@gmail.com>
2025-10-07 07:24:44 -0300

    Estilização do carrinho - parcial

--- Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
+++ Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
@@ -38,9 +38,9 @@ const CartItem = ({ item }: any) => {
 
           <TouchableOpacity
             onPress={() => handleRemove(item)}
-            style={styles.button}
+            style={styles.removeButton}
           >
-            <Text style={styles.buttonText}>Remover</Text>
+            <Text style={styles.removeButtonText}>Remover</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -58,15 +58,46 @@ const styles = StyleSheet.create({
     borderColor: "#ddd",
   },
   image: {
-    width: "50%",
+    width: 100,
     height: 100,
-    borderRadius: 8,
+    borderRadius: 10,
     borderColor: "#ddd",
+    marginRight: 10,
   },
-  name: {},
-  quantity: {},
-  price: {},
-  button: {},
-  buttonText: {},
-  quantityValue: {},
+  name: {
+    fontSize: 16,
+  },
+  quantity: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 5,
+  },
+  price: {
+    fontSize: 14,
+    color: "#007BFF",
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    borderRadius: 5,
+    padding: 5,
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#fff",
+  },
+  quantityValue: {
+    marginHorizontal: 10,
+    fontSize: 16,
+  },
+  removeButton: {
+    marginLeft: 10,
+    backgroundColor: "#FF0000",
+    padding: 10,
+    borderRadius: 5,
+  },
+  removeButtonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
 });
